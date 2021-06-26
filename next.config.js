e66dae2b67e2418c1733d2830c820315fa56b1b1
FrancoAguirre644 <francoaguirre644@gmail.com>
2021-06-26 13:10:02 -0300

    Change BASE_URL

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        "BASE_URL": "https://next-ecommerce-rho.vercel.app",
+        "BASE_URL": "https://next-ecommerce-rho.vercel.app/",
         "MONGODB_URL": "mongodb+srv://francoaguirre644:42385689@cluster0.zgks3.mongodb.net/next_ecommerce?retryWrites=true&w=majority",
         "ACCESS_TOKEN_SECRET": "=}D?1B3GQC%*f{iVUkD:PHTka^|klNtSrt?3@[wlr2~)KG@CoM",
         "REFRESH_TOKEN_SECRET": "CE7nriPi.!4Qr*-R jir#)+Xpmst/VT~oh+>3S7?gVtZ?Nb:vb[sYEQs{M#6P!W",
