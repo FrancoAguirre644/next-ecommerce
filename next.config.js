f86ddc6648e964ed0aa6c409a76991fd78e6a615
FrancoAguirre644 <francoaguirre644@gmail.com>
2024-04-05 02:30:32 -0300

    Change BASE_URL

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        "BASE_URL": "http://localhost:3000",
+        "BASE_URL": "https://next-ecommerce-rho.vercel.app/",
         "MONGODB_URL": "mongodb+srv://francoaguirre644:42385689@cluster0.tbiykuo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
         "ACCESS_TOKEN_SECRET": "=}D?1B3GQC%*f{iVUkD:PHTka^|klNtSrt?3@[wlr2~)KG@CoM",
         "REFRESH_TOKEN_SECRET": "CE7nriPi.!4Qr*-R jir#)+Xpmst/VT~oh+>3S7?gVtZ?Nb:vb[sYEQs{M#6P!W",
