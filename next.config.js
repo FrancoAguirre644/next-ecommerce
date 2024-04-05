6ab36b63760c0520d18c06966122a4cc9710e390
FrancoAguirre644 <francoaguirre644@gmail.com>
2024-04-05 01:39:43 -0300

    Change BASE_URL

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     env: {
         "BASE_URL": "https://next-ecommerce-rho.vercel.app/",
-        "MONGODB_URL": "mongodb+srv://francoaguirre644:42385689@cluster0.zgks3.mongodb.net/next_ecommerce?retryWrites=true&w=majority",
+        "MONGODB_URL": "mongodb+srv://francoaguirre644:<password>@cluster0.tbiykuo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
         "ACCESS_TOKEN_SECRET": "=}D?1B3GQC%*f{iVUkD:PHTka^|klNtSrt?3@[wlr2~)KG@CoM",
         "REFRESH_TOKEN_SECRET": "CE7nriPi.!4Qr*-R jir#)+Xpmst/VT~oh+>3S7?gVtZ?Nb:vb[sYEQs{M#6P!W",
         "PAYPAL_CLIENT_ID": "AUgyKsFrPDSNzbLAkVhfdk_kI1jWhCjvIE3cwLDjig7-9GbTejreVW6keVRs6V_IBBRf_Q0N7a6otVso",
